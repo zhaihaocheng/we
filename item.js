65207a1c614db1ad26a7bd69f963731e05813134
zhaihaocheng <32922602+zhaihaocheng@users.noreply.github.com>
2017-11-01 20:01:27 +0800

    搜索 下午 11/1

--- item.js
+++ item.js
@@ -2,30 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 var mysql      = require('mysql');
-var connection = mysql.createPool({//创建链接地址
-    host     : 'localhost',//IP地址
-    user     : 'root',//用户名
-    password : '',//密码
+var connection = mysql.createPool({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
     database:'taobao'
 });
-
-/* GET home page. */
-
 router.post('/submit', function(req, res, next) {
     console.log("进入后台")
-
     res.header('Access-Control-Allow-Origin','*')
-    var aa=req.body.name;
-    var b=req.body.title
-    connection.query(`INSERT INTO item(name,detail) VALUES('${aa}','${b}')`,function(){
-        res.send("加入成功")
-    }) 
-    /*connection.query(`INSERT INTO item(name,price) VALUES('${aa}','${bb}')`, function() {//err报错rows形参
-        
-    console.log(req.body)
-    var aa=req.body.name;
-    var b=req.body.title
-        res.send("jarsad")
-    });*/
+    var aa=req.body.b;
+    console.log(aa)
+    connection.query(`SELECT * FROM age WHERE name LIKE '%${aa}%'`,function(err,we){
+       console.log(we)
+
+        res.send(we)
+    })
 });
 module.exports = router;
