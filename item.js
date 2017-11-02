1eff107bcb6787edebad69f71e97cfbf85e8aa93
zhaihaocheng <32922602+zhaihaocheng@users.noreply.github.com>
2017-11-02 20:51:58 +0800

    星期四下午表11/2
    星期四11/2

--- item.js
+++ item.js
@@ -1,31 +1,30 @@
 var express = require('express');
 var router = express.Router();
-
 var mysql      = require('mysql');
-var connection = mysql.createPool({//创建链接地址
-    host     : 'localhost',//IP地址
-    user     : 'root',//用户名
-    password : '',//密码
+var connection = mysql.createPool({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
     database:'taobao'
 });
-
-/* GET home page. */
-
-router.post('/submit', function(req, res, next) {
+router.post('/request',function(req, res, next) {
     console.log("进入后台")
-
     res.header('Access-Control-Allow-Origin','*')
-    var aa=req.body.name;
-    var b=req.body.title
-    connection.query(`INSERT INTO item(name,detail) VALUES('${aa}','${b}')`,function(){
-        res.send("加入成功")
-    }) 
-    /*connection.query(`INSERT INTO item(name,price) VALUES('${aa}','${bb}')`, function() {//err报错rows形参
-        
-    console.log(req.body)
-    var aa=req.body.name;
-    var b=req.body.title
-        res.send("jarsad")
-    });*/
+    var a=req.body.title;
+    var b=req.body.na;
+    var c=req.body.te;
+    var d=req.body.texts;
+    var h=req.body.uid;
+    console.log(h)
+    connection.query(`INSERT INTO details(title,time,writer,concent,uid) VALUES('${a}',now(),'${c}','${d}',${h})`,function(err,we){
+        res.send(we)
+    })
+});
+router.post('/gain',function(req, res, next) {
+    console.log("进入后台")
+    res.header('Access-Control-Allow-Origin','*')
+    connection.query('SELECT * FROM details',function(err,we){
+        res.send(we)
+    })
 });
 module.exports = router;
